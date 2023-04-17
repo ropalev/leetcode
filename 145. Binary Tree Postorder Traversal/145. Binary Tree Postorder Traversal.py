7b91b3c336dd76f709713c77dd25d0ac385e378d
Ropalev <opalev89@gmail.com>
2023-04-17 16:28:52 +0300

    Подготовка

---
+++ 145. Binary Tree Postorder Traversal/145. Binary Tree Postorder Traversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from typing import  Optional, List
+
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        dfs(root,res)
+        return res
+
+def dfs(node, res):
+    if node:
+        dfs(node.left,res)
+        dfs(node.right, res)
+        res.append(node.val)
